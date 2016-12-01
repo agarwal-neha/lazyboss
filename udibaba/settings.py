d61711261f68ebd606b001b9815ff9c9ee0920d5
Neha Agarwal <neha.agarwal@hashedin.com>
2016-12-01 23:43:19 +0530

    Adding corsheader and requirements

--- udibaba/settings.py
+++ udibaba/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'templates/static/'),
    os.path.join(BASE_DIR, 'templates/')
 ]
+
+CORS_ORIGIN_WHITELIST = (
+   'localhost:3000'
+)
