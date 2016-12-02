a1456283b8297a61bc93ae731df7c3c1a8f4d47b
Aman Raj <aman.raj@hashedin.com>
2016-12-03 02:28:22 +0530

    Adding view to fetch all players and add new player

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,6 @@ import views
 urlpatterns = [
     url(r'^index/$', views.index),
     url(r'^place/', views.place_bet),
+    url(r'^players/', views.get_all_players),
+    url(r'^player/', views.add_player),
 ]
