db2896f6f1c561790f30d5f146a681ff6e8d254d
Vaibhav Singh <vaibhav.singh@hashedin.com>
2016-12-02 19:59:26 +0530

    everything

---
+++ static/udibaba/src/Container/ProfileContainer.js
@@ -0,0 +1,9 @@
+import {ProfileNav} from '../Component/ProfileNav'
+import React,{Component} from 'react'
+
+
+export class ProfilePage extends Component {
+    render() {
+        return (<ProfileNav/>)
+    }
+}
